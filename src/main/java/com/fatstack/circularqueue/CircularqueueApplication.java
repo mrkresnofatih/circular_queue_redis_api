302878be4d9a3c1d644d4413f680d2ef42a56635
Kresno Fatih Imani <90560422+mrkresnofatih@users.noreply.github.com>
2022-03-02 07:07:28 +0700

    Update CircularqueueApplication.java

--- src/main/java/com/fatstack/circularqueue/CircularqueueApplication.java
+++ src/main/java/com/fatstack/circularqueue/CircularqueueApplication.java
@@ -3,6 +3,7 @@ package com.fatstack.circularqueue;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+// start
 @SpringBootApplication
 public class CircularqueueApplication {
 
